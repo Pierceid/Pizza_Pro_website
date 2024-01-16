5eaa98534923fa838c0833f43893b1c24d3a307f
Pierceid <128907409+Pierceid@users.noreply.github.com>
2024-01-16 20:35:21 +0100

    optimizing carousel

--- public/js/script_shop.js
+++ public/js/script_shop.js
@@ -9,27 +9,27 @@ document.addEventListener('DOMContentLoaded', function () {
     prev.addEventListener('click', slidePrev);
 
     function slideNext() {
-        images[counter].style.animation = 'next1 0.5s ease-in-out forwards';
+        images[counter].style.animation = 'next1 0.4s ease-in-out forwards';
         if (counter >= images.length - 1) {
             counter = 0;
         } else {
             counter++;
         }
-        images[counter].style.animation = 'next2 0.5s ease-in-out forwards';
+        images[counter].style.animation = 'next2 0.4s ease-in-out forwards';
     }
 
     function slidePrev() {
-        images[counter].style.animation = 'prev1 0.5s ease-in-out forwards';
+        images[counter].style.animation = 'prev1 0.4s ease-in-out forwards';
         if (counter <= 0) {
             counter = images.length - 1;
         } else {
             counter--;
         }
-        images[counter].style.animation = 'prev2 0.5s ease-in-out forwards';
+        images[counter].style.animation = 'prev2 0.4s ease-in-out forwards';
     }
 
     function autoSliding() {
-        interval = setInterval(timer, 1200);
+        interval = setInterval(timer, 1400);
 
         function timer() {
             slideNext();
