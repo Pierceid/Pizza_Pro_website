ee569bd703e11ccd3a2f6e1f600b5c5d67437a05
Pierceid <128907409+Pierceid@users.noreply.github.com>
2024-01-25 14:33:08 +0100

    improved animation

--- public/js/script_secondary.js
+++ public/js/script_secondary.js
@@ -2,6 +2,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// animation
 document.addEventListener("DOMContentLoaded", function () {
     const numberOfPizzas = 10;
 
@@ -9,18 +10,22 @@ document.addEventListener("DOMContentLoaded", function () {
         const pizza = document.createElement("img");
         pizza.classList.add("pizza");
         pizza.src = "/public/images/others/logo.png";
-        pizza.style.left = getRandomInt(10, 90) + "vw";
-        pizza.style.top = getRandomInt(10, 90) + "vh";
+        pizza.style.left = getRandomInt(20, 80) + "vw";
+        pizza.style.top = getRandomInt(20, 80) + "vh";
         pizza.style.animationName = `move-pizza-${i}`;
         document.body.appendChild(pizza);
 
         const keyframes = `@keyframes move-pizza-${i} {
-        0% { transform: translate(0, 0); }
-        25% { transform: translate(${getRandomInt(-150, 150)}px, ${getRandomInt(-150, 150)}px); }
-        50% { transform: translate(${getRandomInt(-150, 150)}px, 0); }
-        75% { transform: translate(${getRandomInt(-150, 150)}px, ${getRandomInt(-150, 150)}px); }
-        100% { transform: translate(0, 0); }
-      }`;
+            0% { transform: translate(0, 0); }
+            12% { transform: translate(${getRandomInt(-150, 150)}px, ${getRandomInt(-150, 150)}px); }
+            25% { transform: translate(${getRandomInt(-150, 150)}px, 0); }
+            37% { transform: translate(${getRandomInt(-150, 150)}px, ${getRandomInt(-150, 150)}px); }
+            50% { transform: translate(0, ${getRandomInt(-150, 150)}px); }
+            62% { transform: translate(${getRandomInt(-150, 150)}px, ${getRandomInt(-150, 150)}px); }
+            75% { transform: translate(${getRandomInt(-150, 150)}px, 0); }
+            87% { transform: translate(${getRandomInt(-150, 150)}px, ${getRandomInt(-150, 150)}px); }
+            100% { transform: translate(0, 0); }
+        }`;
 
         const style = document.createElement("style");
         style.position = 'relative';
@@ -28,5 +33,18 @@ document.addEventListener("DOMContentLoaded", function () {
         style.height = '50px';
         style.textContent = keyframes;
         document.head.appendChild(style);
+
+        // Check if the pizza goes off screen
+        pizza.addEventListener('animationiteration', function () {
+            const rect = pizza.getBoundingClientRect();
+            if (
+                rect.bottom < 0 || rect.top > window.innerHeight ||
+                rect.right < 0 || rect.left > window.innerWidth
+            ) {
+                pizza.style.visibility = 'hidden';
+            } else {
+                pizza.style.visibility = 'visible';
+            }
+        });
     }
 });
